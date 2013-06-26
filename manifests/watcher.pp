67d5d098df2c6f1fad13a48dd06b0712de2a0999
Martin Filliau <martin@filliau.com>
2013-06-26 10:54:54 +0100

    Added options max_bytes and backup_count to FileStream

--- manifests/watcher.pp
+++ manifests/watcher.pp
@@ -2,6 +2,8 @@ define circus::watcher($cmd, $args, $warmup_delay = 0, $numprocesses = 1,
                        $uid = "root", $gid = "root", $env = "", $priority = 0,
                         $stdout_class = "", $stdout_filename = "",
                         $stderr_class = "", $stderr_filename = "",
+                        $stdout_file_max_bytes = "10485760", $stdout_file_backup_count = "10",
+                        $stderr_file_max_bytes = "10485760", $stderr_file_backup_count = "10",
                         $send_hup = false, $working_dir = "") {
     include circus::manager
 
